71738a4b15cb0d74d7c3c8af4d3e8473592033ab
FIyingIdeal <FlyingIdeal_yan@163.com>
2019-03-13 22:38:32 +0800

    'spring-cloud-study'

---
+++ spring-cloud-config-server/src/main/java/com/flyingideal/springcloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.flyingideal.springcloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yanchao
+ * @date 2019/3/13 10:27
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
